62f0104e5f4dcb1178753482c586c5cb31c46fad
shaily <shaily@IBM009-PC0GJ0G8.in.ibm.com>
2017-02-21 16:24:24 -0500

    add

--- finished/chaincode_finished.go
+++ finished/chaincode_finished.go
@@ -94,7 +94,6 @@ func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, args []string)
 	b=args[1]
 	key = args[2]
 	sum := Sum(a,b)
-	fmt.println("Sum==",sum)
 	
 	//s := []string{value, " From Shaily"}
 	//s1 := strings.Join(s, ",")
@@ -119,10 +118,9 @@ func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) 
 		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
 	}
 
-	key = args[0]
+	key = args[0]//keys to read
 	//key = args[2]
 	valAsbytes, err := stub.GetState(key)
-	fmt.
 	if err != nil {
 		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
 		return nil, errors.New(jsonResp)
