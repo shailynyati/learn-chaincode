a1ea1ff44f1f3a5fa84f71ff46b6373bdfbbc902
shailynyati <shailykuiya@gmail.com>
2017-05-08 12:44:56 +0530

    fix

--- finished/chaincode_finished.go
+++ finished/chaincode_finished.go
@@ -74,11 +74,11 @@ func (t *SimpleChaincode) AddDeletePoints(stub shim.ChaincodeStubInterface, args
 	var totalPoints int
 	ffId := args[0]
 	operator := args[1]
-	points []byte := t.getPoints(stub, args)
+	points := t.getPoints(stub, args)
 	userAsbytes := t.getUser(stub, args)
 	user := UserRegistrationDetails{}
 	user_json := json.Unmarshal(userAsbytes, &user)
-	totalPoints = strconv.Atoi(user_json.TotalPoints)
+	totalPoints = []byte strconv.Atoi(user_json.TotalPoints)
 
 	if operator == "Add" {
 		totalPoints += points
